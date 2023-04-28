42769028c057fb9a1ae43d0d1c7e4df6167e6128
Jack Briggs <mrjackthebriggs@gmail.com>
2023-04-28 17:30:06 +0800

    Merge branch '28-04-26-5-02pm' of https://github.com/mrjackthebriggs/ECUTEST2804

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import randomarray
 # Comments?
 if __name__ == "__main__":
     print("10 random arrays that only use 1 to 10 once")
-    array = randomarray.rand_seq()
+    array = randomarray.rand_seq(10)
     for i in array:
         print(i)
     
