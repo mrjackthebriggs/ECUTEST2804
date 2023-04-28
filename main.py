3fe2d6c2f2fd4dc957357eb1654f1bb0df794739
Jack Briggs <mrjackthebriggs@gmail.com>
2023-04-28 16:30:09 +0800

    Added Comments  Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import randomarray
 
+# Comments?
 if __name__ == "__main__":
     print("10 random arrays that only use 1 to 10 once")
     array = randomarray.rand_seq()
     for i in array:
-        print(i);
+        print(i)
     
